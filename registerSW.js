f11828154f379cc1d4cee5ddabc64dcf41e0171c
beyondworks <beyondworks.br@gmail.com>
2025-06-01 01:14:20 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/beyond-order-hub-frontend/sw.js', { scope: '/beyond-order-hub-frontend/' })})}
